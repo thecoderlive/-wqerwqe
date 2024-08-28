7aa103c7dc56e7b7e11597cfbd40301af62ce14f
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2024-08-29 03:51:32 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "b54b0f9c-9d13-4f9f-afb0-5460978d921d",
+    "home_feed": [
+        {
+            "id": "a98b8130-0f51-4dcf-9a1c-88fa4f4b2c34",
+            "artist_name": "too",
+            "home_carousel": [
+                {
+                    "id": "ac45839b-4335-4112-bd4b-31a205486480",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/161.png"
+                },
+                {
+                    "id": "dc658633-be61-4082-9406-202a1c80b4f2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/94.png"
+                },
+                {
+                    "id": "b80e307e-516a-44a0-b1d1-9556c295c60b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/182.png"
+                },
+                {
+                    "id": "1becc19b-779a-4f2a-8d83-fbed92d4af7f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/1.png"
+                },
+                {
+                    "id": "588bf239-a36e-4276-8b72-da6f495d2f77",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/232.png"
+                }
+            ],
+            "art_caption": "sister",
+            "art_description": "Join day serve wife job land. Democrat identify ground like."
+        },
+        {
+            "id": "e1080e5f-cc8d-405c-b56f-a986eb17a4c6",
+            "artist_name": "statement",
+            "home_carousel": [
+                {
+                    "id": "a925636a-f95e-4303-8cd0-947b7b5197a3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/93.png"
+                },
+                {
+                    "id": "15c59174-3640-4fb9-830d-b6555dde7463",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/28.png"
+                },
+                {
+                    "id": "7dc7202c-9bde-4c85-9513-451a6d0240dd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/119.png"
+                },
+                {
+                    "id": "5bfcb01d-0978-4264-844d-6147448e135e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/150.png"
+                },
+                {
+                    "id": "52266c97-63b9-4e3c-a48e-6af921daf625",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/342.png"
+                }
+            ],
+            "art_caption": "employee",
+            "art_description": "Hard attack end outside low camera. Put make tree set. Down agree behavior recently."
+        },
+        {
+            "id": "12ff1f69-e017-4a34-9c8a-7c845ceeb3c1",
+            "artist_name": "leave",
+            "home_carousel": [
+                {
+                    "id": "c1a45679-9d1f-4820-af5f-63de747466b1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/333.png"
+                },
+                {
+                    "id": "e740a629-53ea-477c-a198-5627d8b4255c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/50.png"
+                },
+                {
+                    "id": "a392daa9-9c82-4d14-94aa-8b525e8ec43a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/329.png"
+                },
+                {
+                    "id": "7d4b7b58-d535-4df1-b9cb-7c9225d4dd61",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/131.png"
+                },
+                {
+                    "id": "b79edc0d-883d-4dfc-8a85-1e01d890907b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/34.png"
+                }
+            ],
+            "art_caption": "establish",
+            "art_description": "Growth six purpose far range film. Have increase child four. Network still kitchen sign."
+        },
+        {
+            "id": "d49c8ac8-a265-41ed-93d6-2f7386568651",
+            "artist_name": "however",
+            "home_carousel": [
+                {
+                    "id": "a918e823-06ee-4ee5-bd86-69be1366be44",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/225.png"
+                },
+                {
+                    "id": "7a47fb1a-ec83-4dd3-be6e-38f2a54e0505",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/300.png"
+                },
+                {
+                    "id": "6fd32f68-b6e6-4540-9aef-bda024957d58",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/132.png"
+                },
+                {
+                    "id": "b7031678-1b97-49fe-bf44-c5b1dc07903d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/91.png"
+                },
+                {
+                    "id": "980c8d9b-3f88-478f-be43-6bee8a5a4832",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/42.png"
+                }
+            ],
+            "art_caption": "character",
+            "art_description": "Month financial ready work without car actually that. Foot ask father."
+        },
+        {
+            "id": "7e716fce-be0e-4555-9631-6269db3484f6",
+            "artist_name": "condition",
+            "home_carousel": [
+                {
+                    "id": "3776177a-829b-45a6-bc52-8d16068c6fac",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/133.png"
+                },
+                {
+                    "id": "560bf0b5-1aa2-4abd-891d-5b9150f53342",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/194.png"
+                },
+                {
+                    "id": "c6c805a3-441f-47c8-8d16-f45cdb10cc42",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/99.png"
+                },
+                {
+                    "id": "2b7c76d7-e509-4d1c-aa57-90186c93b493",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/265.png"
+                },
+                {
+                    "id": "f5b3163d-7025-44d1-b182-ec86aeb02437",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/182.png"
+                }
+            ],
+            "art_caption": "want",
+            "art_description": "Fact head building appear send. Measure prepare ability sister staff describe. Else thousand mention center."
+        }
+    ]
+}
